865a3dbe84f139c764cedbf24e2b758c4c3750be
Hole <83029206+HoleForYou@users.noreply.github.com>
2021-09-02 00:37:24 +0530

    INfinite scrolling added
    Now I have added infinite scrolling and random platform generation into the code.
    
    This way when the player jumps up new platforms are made randomly and the game keeps going.
    
    It's not complete yet I have to make it look nicer because right now the platforms are very close to each other. But i will learn that tomorrow.

--- september1.py
+++ september1.py
@@ -9,6 +9,8 @@ from pygame.locals import (
     KEYDOWN,
     QUIT,
 )
+import sys
+import random
 
 # initialisation
 py.init()
@@ -77,15 +79,35 @@ class player(py.sprite.Sprite):
 class platform(py.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.surf = py.Surface((width, 20))
+        self.surf = py.Surface((random.randint(50, 100), 12))
         self.surf.fill((0, 150, 255))
-        self.rect = self.surf.get_rect(center=(width / 2, height - 10))
+        self.rect = self.surf.get_rect(center=(random.randint(
+            0, width - 10), random.randint(0, height - 30)))
 
+    def move(self):
+        pass
+
+# Randomly generating platforms on screen to make the game infiniteb
+
+
+def plat_gen():
+    while len(platforms) < 7:
+        plat_gen_width = random.randrange(50, 500)
+        p = platform()
+        p.rect.center = (random.randrange(0, plat_gen_width),
+                         random.randrange(-50, 0))
+        platforms.add(p)
+        all_sprites.add(p)
 
 
 # Sprites
 pt1 = platform()
 p1 = player()
+
+# Platform code
+pt1.surf = py.Surface((width, 20))
+pt1.surf.fill((0, 150, 255))
+pt1.rect = pt1.surf.get_rect(center=(width / 2, height - 10))
 
 # Sprite Group
 all_sprites = py.sprite.Group()
@@ -95,21 +117,42 @@ all_sprites.add(p1)
 platforms = py.sprite.Group()
 platforms.add(pt1)
 
+# Random platform generating code in the beginning of the game
+for x in range(random.randint(5, 6)):
+    pl = platform()
+    platforms.add(pl)
+    all_sprites.add(pl)
+    print("hello pygame")
+
+
 # Game Loop
 running = True
 while running:
+    # for getting the key presses
     for event in py.event.get():
+        # For closing the game
         if event.type == QUIT:
             running = False
         elif event.type == KEYDOWN:
             if event.key == K_ESCAPE:
                 running = False
+        # The jump feature
         if event.type == py.KEYDOWN:
             if event.key == py.K_SPACE:
                 p1.jump()
 
+    # Infinite scrolling code
+    if p1.rect.top <= height / 3:
+        p1.pos.y += abs(p1.vel.y)
+        for plat in platforms:
+            plat.rect.y += abs(p1.vel.y)
+            if plat.rect.top >= height:
+                plat.kill()
+
     display_surface.fill((0, 0, 0))
 
+    # generate the platforms
+    plat_gen()
     p1.move()
     p1.update()
     for entity in all_sprites:
